107b5850a2cce9638f5b8d96c7d6fa089926d809
Gurasees Singh <96414381+inAsees@users.noreply.github.com>
2022-02-25 21:57:51 +0530

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -1,46 +1,34 @@
 import random
 
 
-def guessGame(num):
-    print(f'Guess a number between 1 and {num}.You have got 3 attempts to guess!!')
-    user_guess = int(input('Enter the number :'))
-    comp_choose = random.randint(1, num)
-    attempts = 1
-    while user_guess != comp_choose and attempts < 3:
-        if user_guess < 1 or user_guess > num:
-
-            try:
-                user_guess = int(
-                    input(f'Entered number is out of range,please guess a number between 1 and {num} again:\n'))
-            except Exception:
-                user_guess = int(
-                    input(f'Invalid number!! Try again:\n'))
-            attempts += 1
-        elif user_guess < comp_choose:
-
-            try:
-                user_guess = int(input('number is SMALL , guess BIGGER number again!\nTry again:'))
-            except Exception:
-                user_guess = int(input(f'Invalid number!! Try again:\n'))
-            attempts += 1
-        else:
-
-            try:
-                user_guess = int(input('number is BIG , guess SMALLER number again!\nTry again:'))
-            except Exception:
-                user_guess = int(input(f'Invalid number!!\nTry again:'))
-            attempts += 1
-        if attempts == 1:
+def guessGame(upper_bound):
+    print(f'Guess a number between 1 and {upper_bound}.You have got 3 attempts to guess!!')
+    comp_choose = random.randint(1, upper_bound)
+    attempts = 0
+    while attempts < 3:
+        attempts += 1
+        try:
+            user_guess = int(input('Enter the number :'))
+        except Exception:
+            print('Invalid input, try again\n')
+            attempts -= 1
+            continue
+        if comp_choose == user_guess:
             print(f'Number {comp_choose} found in {attempts} attempt.')
-        elif attempts == 3:
-            print(
-                'You are out of total attempts!! Better luck next time.\n Do you want to play again!! For YES/NO press Y/N\n')
-            play_again = input().upper()
-            if play_again == 'Y':
-                guessGame(2)
+            break
 
+        elif user_guess < 1 or user_guess > upper_bound:
+            print(f'Entered number is out of range,please guess a number between 1 and {upper_bound} again:')
+        elif user_guess < comp_choose:
+            print('number is SMALL , guess BIGGER number again!')
         else:
-            print(f'Number {comp_choose} found in {attempts} attempts.')
+            print('number is BIG , guess SMALLER number again!')
+
+    print(
+        'You are out of total attempts!! Better luck next time.\nDo you want to play again!! For YES/NO press Y/N\n')
+    play_again = input().upper()
+    if play_again == 'Y':
+        guessGame(12)
 
 
-guessGame(2)
+guessGame(12)
