ed8cf06fbaae75b0db8ddd0b3278878f357c4420
anmoluppal <anmoluppal365@gmail.com>
2022-03-03 23:20:21 +0530

    Implement alternate WordGuess game.

--- word_guess.py
+++ word_guess.py
@@ -66,6 +66,30 @@ class WordGuess:
         return self._correct_attempt == 0 and self._wrong_attempt == self._max_attempts // 2
 
 
+class WordGuess2:
+    def __init__(self, max_attempts: int):
+        self._max_attempts = max_attempts
+        self._curr_attempt = 0
+        self._random_word = random.choice(['hang', 'dare', 'truth', 'buffalo', 'peace', 'hello'])
+        self._guess_characters = ["" for i in range(len(self._random_word))]
+
+    def get_random_word(self) -> str:
+        return self._random_word
+
+    def register_user_input(self, char: str):
+        for i in range(len(self._random_word)):
+            if char == self._random_word[i]:
+                self._guess_characters[i] = char
+
+        self._curr_attempt += 1
+
+    def is_attempts_left(self):
+        return self._curr_attempt < self._max_attempts
+
+    def is_word_guessed(self) -> bool:
+        return self._random_word == "".join(self._guess_characters)
+
+
 class CliHandler:
     def __init__(self, max_attempts):
         self._word_guess = WordGuess(max_attempts)
