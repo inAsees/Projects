a68d2284b43c1e887f8f78c2b99262b9f018c95d
Gurasees Singh <gurasees.singh121@gmail.com>
2022-03-07 16:23:01 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -2,14 +2,20 @@ class LeapYear:
     def __init__(self, year_input: int):
         self._year_input = year_input
 
-    def is_year_input_century_year(self) -> bool:
+    def _is_year_input_century_year(self) -> bool:
         return self._year_input % 100 == 0
 
-    def is_century_year_equal_leap_year(self) -> bool:
+    def _is_century_year_equal_leap_year(self) -> bool:
         return self._year_input % 400 == 0
 
-    def is_normal_year_equal_leap_year(self) -> bool:
+    def _is_normal_year_equal_leap_year(self) -> bool:
         return self._year_input % 4 == 0
+
+    def is_leap_year(self):
+        if self._is_year_input_century_year():
+            return self._is_century_year_equal_leap_year()
+        else:
+            return self._is_normal_year_equal_leap_year()
 
 
 class CliHandler:
@@ -22,12 +28,7 @@ class CliHandler:
                 print("Invalid input.\tTry again.")
                 continue
             leap_year = LeapYear(year_input)
-            if leap_year.is_year_input_century_year():
-                print(leap_year.is_century_year_equal_leap_year())
-                break
-            else:
-                print(leap_year.is_normal_year_equal_leap_year())
-                break
+            print(leap_year.is_leap_year())
 
 
 if __name__ == "__main__":
