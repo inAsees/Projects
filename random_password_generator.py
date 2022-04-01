1d8a95c76716d9f5e5c73e9ea196bc9636283596
Gurasees Singh <gurasees.singh121@gmail.com>
2022-04-01 15:16:07 +0530

    Update random_password_generator.py

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,5 +1,7 @@
 import random
 import string
+import os
+import csv
 
 
 class PasswordGenerator:
@@ -11,29 +13,44 @@ class PasswordGenerator:
         if password_length != 0:
             for i in range(password_length):
                 password += random.choice(string.ascii_letters + string.punctuation + string.digits)
-            return password
 
         else:
             password_length = random.randint(self._minimum_length, 12)
             for i in range(password_length):
                 password += random.choice(string.ascii_letters + string.punctuation + string.digits)
-            return password
 
-    def get_minmum_length(self) -> int:
+        return password
+
+    def create_directory(self) -> None:
+        if "My passwords repository" not in os.listdir(r'C:\Users\DELL\Desktop'):
+            os.mkdir(r"C:\Users\DELL\Desktop\My passwords repository")
+        
+    def save_password_in_file(self, password_usage: str, password: str):
+        if f"{password_usage}_password.csv" not in os.listdir(
+                r"C:\Users\DELL\Desktop\My passwords repository"):
+            with open(
+                    rf"C:\Users\DELL\Desktop\My passwords repository\{password_usage}_password.csv",
+                    "w") as f:
+                writer = csv.writer(f)
+                writer.writerow([f"{password_usage}", f"{password}"])
+
+    def get_minimum_length(self) -> int:
         return self._minimum_length
 
 
 class CliHandler:
 
     def start(self):
+
         password_gen = PasswordGenerator()
+        password_gen.create_directory()
         password_usage = input("Password Generator Application.\n"
                                "Where are you going to use this password? "
                                "Example:Facebook,Instagram etc.\n")
         while True:
             user_input = input(
                 f"Do you want to provide maximum length for the password?\n"
-                f"Make sure to provide minimum length of {password_gen.get_minmum_length()} characters.\n"
+                f"Make sure to provide minimum length of {password_gen.get_minimum_length()} characters.\n"
                 "If YES press '1'.\n"
                 "If NO press '2'.\n")
             if user_input == "1":
@@ -44,19 +61,25 @@ class CliHandler:
                         print("Invalid input")
                         continue
 
-                    if password_length < password_gen.get_minmum_length():
-                        print(f"This is less than {password_gen.get_minmum_length()},try again.")
+                    if password_length < password_gen.get_minimum_length():
+                        print(f"This is less than {password_gen.get_minimum_length()},try again.")
                         continue
+                    password = password_gen.generate_password(password_length)
+                    password_gen.save_password_in_file(password_usage, password)
                     print(f"Password is: {password_gen.generate_password(password_length)}")
                     break
                 break
 
             elif user_input == "2":
-                print(f"Password is: {password_gen.generate_password(password_length=0)}")
+                password = password_gen.generate_password(password_length=0)
+                password_gen.save_password_in_file(password_usage, password)
+                print(f"Password is: {password}")
                 break
             else:
                 print("Invalid input")
                 continue
+
+        pass
 
 
 if __name__ == "__main__":
