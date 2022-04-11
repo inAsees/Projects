4a4c07676b191cf31b87624d1b7f66e0094ebce1
Gurasees Singh <gurasees.singh121@gmail.com>
2022-04-11 16:29:07 +0530

    Update ContactBook.py

--- ContactBook.py
+++ ContactBook.py
@@ -1,100 +1,108 @@
-import pickle
 import os
+import csv
+import pandas as pd
 
 
 class ContactBook:
     def __init__(self):
+        self.create_contact_book()
+
+    @staticmethod
+    def create_contact_book():
+        if 'contact_book.csv' not in os.listdir(os.getcwd()):
+            with open('contact_book.csv', 'w') as f:
+                header = csv.DictWriter(f, fieldnames=["Name", "Contact number", "Email Id", "Address"])
+                header.writeheader()
+
+    @staticmethod
+    def add_contact(name, contact_number, email_id, address) -> None:
+        df = pd.DataFrame([[name, contact_number, email_id, address]])
+        df.to_csv(r"C:\Users\DELL\PycharmProjects\TestProject\Beginner projects in Python\contact_book.csv", mode='a',
+                  header=False, index=False)
+
+    @staticmethod
+    def edit_contact(name):
         pass
 
-    def create_contact_book(self):
-        with open('contact_book.pkl', 'wb') as f:
-            pickle.dump([], f)
-
-    def add_contact(self, first_name, surname, contact_number, email, address):
-        with open('contact_book.pkl', 'rb+') as f:
-            contacts = pickle.load(f)
-            contacts.append([first_name + ' ' + surname, contact_number, email, address])
-            pickle.dump(contacts, f)
-
-    def edit_contact(self):
-        with open('contact_book.pkl', 'rb+') as f:
-            contacts = pickle.load(f)
+    def delete_contact(self, user_input):
+        with open('contact_book.csv', 'rb+') as f:
             pass
 
-    def delete_contact(self, user_input) -> bool:
-        with open('contact_book.pkl', 'rb+') as f:
-            contacts = pickle.load(f)
-            for index in range(len(contacts)):
-                if user_input == contacts[index][0]:
-                    contacts.pop(index)
-                    break
-                return True
-        
-        return False
-
-    def search_contact(self, name_search):
-        with open('contact_book.pkl', 'rb+') as f:
-            contacts = pickle.load(f)
-            for index in range(len(contacts)):
-                if name_search == contacts[index][0].lower():
-                    return contacts[index]
-        return None
+    @staticmethod
+    def search_contact(name) -> list:
+        df = pd.read_csv(r"C:\Users\DELL\PycharmProjects\TestProject\Beginner projects in Python\contact_book.csv")
+        arr = df.values
+        for row in arr:
+            if row[0] == name:
+                return row
 
     def display_contact_book(self):
-        with open('contact_book.pkl', 'rb') as f:
-            contacts = pickle.load(f)
-            for index in range(len(contacts)):
-                pass
+        pass
 
-    def is_file_in_current_working_dir(self) -> bool:
-        return 'contact_book.pkl' in os.getcwd()
+    @staticmethod
+    def is_name_in_contact_book(name) -> bool:
+        df = pd.read_csv(r"C:\Users\DELL\PycharmProjects\TestProject\Beginner projects in Python\contact_book.csv")
+        return name in list(df.Name)
 
 
 class CliHandler:
     def __init__(self):
-        self._contact_book_obj = ContactBook()
+        self._contact_book = ContactBook()
 
     def start(self):
-        if not self._contact_book_obj.is_file_in_current_working_dir():
-            self._contact_book_obj.create_contact_book()
 
+        print(
+            'This is your Contact book.\n'
+            'If you want to add a contact press "1"\n'
+            'If you want to edit a contact press "2"\n'
+            'If you want to delete a contact press "3"\n'
+            'If you want to search for contact press "4"\n'
+            'If you want to display your contact list press "5"')
         while True:
-            print(
-                'This is your Contact book.\nIf you want to add a contact press "1"\n'
-                'If you want to edit a contact press "2"\nIf you want to delete a contact press "3"\n'
-                'If you want to search for contact press "4"\nIf you want to display your contact list press "5"')
-
             user_input = int(input('Enter your response:'))
             if user_input == 1:
-                first_name = input('First name:').capitalize()
-                surname = input('Surname:').capitalize()
-                contact_number = input('mobile number:')
-                email = input('email:')
-                address = input('address:')
-                self._contact_book_obj.add_contact(first_name, surname, contact_number, email, address)
+                name = input('Name:')
+                if self._contact_book.is_name_in_contact_book(name):
+                    print("Name already exists in the contact book,please use different name.")
+                    continue
+                contact_number = input('Mobile number:')
+                email_id = input('Email Id:')
+                address = input('Address:')
+                self._contact_book.add_contact(name, contact_number, email_id, address)
                 print('You contact has been successfully created.')
                 break
+
             elif user_input == 2:
-                break
-            elif user_input == 3:
-                user_input = input('enter the name of the contact you want to delete:')
-                if self._contact_book_obj.delete_contact(user_input):
-                    print('Contact is deleted successfully.')
-                else:
-                    print('Contact not found')
-                break
-            elif user_input == 4:
-                name_search = input('Enter the name:').lower()
-                contacts = self._contact_book_obj.search_contact(name_search)
-                if contacts == None:
-                    print('Name not found')
+                name = input('Name:')
+                if self._contact_book.is_name_in_contact_book(name):
+                    contact = self._contact_book.search_contact(name)
+                    print(f"Name:{contact[0]}\n"
+                          f"Contact Number:{contact[1]}\n"
+                          f"Email Id:{contact[2]}\n"
+                          f"Address:{contact[3]}")
+                    pass
                     break
-                print(
-                    f'\tName = {contacts[0]}\n\tContact number = {contacts[1]}\n\tEmail: {contacts[2]}\n'
-                    f'\tAddress = {contacts[3]}')
-                break
+                print("Name does not exist")
+                continue
+            elif user_input == 3:
+                name = input('Name:')
+                if self._contact_book.is_name_in_contact_book(name):
+                    pass
+                    break
+                print("Name does not exist")
+                continue
+            elif user_input == 4:
+                name = input('Enter the name:').lower()
+                if self._contact_book.is_name_in_contact_book(name):
+                    contact = self._contact_book.search_contact(name)
+                    print(f"Name:{contact[0]}\n"
+                          f"Contact Number:{contact[1]}\n"
+                          f"Email Id:{contact[2]}\n"
+                          f"Address:{contact[3]}")
+                    break
+                print("Name does not exist")
+                continue
             elif user_input == 5:
-                self._contact_book_obj.display_contact_book()
                 break
 
 
