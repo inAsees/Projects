a9a165bbe813eacf6571be7758d62674d90bd5bc
Gurasees Singh <gurasees.singh121@gmail.com>
2022-04-14 11:57:10 +0530

    Update contact_book_api.py
    1. Remaining APIs created.
    2. /api/display_contact_book as of now is incomplete.

--- contact_book_api.py
+++ contact_book_api.py
@@ -1,26 +1,60 @@
 from flask import Flask, request
-
 from contact_book import ContactBook
 
 app = Flask(__name__)
-contact_book = ContactBook("/home/anmol/development/misc/Programs/contact_book.csv")
-
-
-@app.route('/api/is_name_in_contact_book', methods=["GET"])
-def is_name_in_contact_book():
-    res = contact_book.is_name_in_contact_book(request.json["name"])
-
-    return {"status": res}
+contact_book = ContactBook(r"C:\Users\DELL\PycharmProjects\contact_book\contact_book.csv")
 
 
 @app.route("/api/add_contact", methods=["POST"])
 def add_contact():
     data = request.json
+    if contact_book.is_name_in_contact_book(data["name"]):
+        return {"status": "Name already exits,try different name."}, 400
+
     try:
         contact_book.add_contact(data["name"], data["contact_number"], data["email_id"], data["address"])
-        return {"status": "OK"}, 200
+        return {"status": "Contact added successfully"}, 200
     except Exception:
         return {"error": "Some error occurred"}, 400
+
+
+@app.route('/api/edit_contact', methods=["POST"])
+def edit_contact():
+    data = request.json
+    if not contact_book.is_name_in_contact_book(data["name"]):
+        return {"status": "Name does not exist."}, 400
+
+    try:
+        contact_book.edit_contact(data["name"], data["new_name"], data["new_contact_number"], data["new_email_id"],
+                                  data["new_address"])
+        return {"status": "Contact updated successfully"}
+    except Exception:
+        return {"error": "Some error occurred"}, 400
+
+
+@app.route('/api/delete_contact', methods=["POST"])
+def delete_contact():
+    data = request.json
+    if not contact_book.is_name_in_contact_book(data["name"]):
+        return {"status": "Name does not exist."}, 400
+    contact_book.delete_contact(data["name"])
+    return {"status": "Contact deleted successfully."}
+
+
+@app.route('/api/search_contact', methods=["POST"])
+def search_contact():
+    data = request.json
+    if not contact_book.is_name_in_contact_book(data["name"]):
+        return {"status": "Name does not exists."}
+    contact = contact_book.search_contact(data["name"])
+    return contact[0]
+
+
+@app.route('/api/display_contact_book', methods=["GET"])
+def display_contact_book():
+    contact = contact_book.display_contact_book()
+    for row in contact:  # How to return all the elements of the list?
+        return row
 
 
 if __name__ == '__main__':
