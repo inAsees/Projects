4778e9a988d9e2768daed8998a9e7089278aa211
Gurasees Singh <gurasees.singh121@gmail.com>
2022-04-20 13:02:30 +0530

    Update contact_book_cli.py
    1. Some minor changes.

--- contact_book_cli.py
+++ contact_book_cli.py
@@ -4,7 +4,7 @@ from contact_book import ContactBook
 class CliHandler:
     def __init__(self):
         self._contact_book = ContactBook(
-            r"C:\Users\DELL\PycharmProjects\TestProject\Beginner projects in Python\contact_book.csv")
+            r"C:\Users\DELL\PycharmProjects\contact_book\contact_book.csv")
 
     @staticmethod
     def _if_user_wants_to_edit_contact() -> bool:
@@ -24,8 +24,9 @@ class CliHandler:
             'If you want to search for contact press "4"\n'
             'If you want to display your contact list press "5"')
         while True:
-            user_input = int(input('Enter your response:'))
-            if user_input == 1:
+            user_input = input('Enter your response:')
+            print("-------------------------------")
+            if user_input == "1":
                 name = input('Name:')
                 if self._contact_book.is_name_in_contact_book(name):
                     print("Name already exists in the contact book,please use different name.")
@@ -36,7 +37,7 @@ class CliHandler:
                 self._contact_book.add_contact(name, contact_number, email_id, address)
                 print('You contact has been successfully created.')
                 break
-            elif user_input == 2:
+            elif user_input == "2":
                 name = input('Name: ')
                 if not self._contact_book.is_name_in_contact_book(name):
                     print("Name does not exist")
@@ -70,7 +71,7 @@ class CliHandler:
                 self._contact_book.edit_contact(name, new_name, new_contact_number, new_email_id, new_address)
                 print("Contact updated successfully")
                 break
-            elif user_input == 3:
+            elif user_input == "3":
                 name = input('Name:')
                 if not self._contact_book.search_contact(name):
                     print("Name does not exist")
@@ -78,7 +79,7 @@ class CliHandler:
                 self._contact_book.delete_contact(name)
                 print("Contact deleted successfully.")
                 break
-            elif user_input == 4:
+            elif user_input == "4":
                 name = input('Enter the name:\n').lower()
                 if not self._contact_book.is_name_in_contact_book(name):
                     print("Name does not exist")
@@ -89,13 +90,14 @@ class CliHandler:
                       f"Email Id:{contact[0]['email_id']}\n"
                       f"Address:{contact[0]['address']}")
                 break
-            elif user_input == 5:
+            elif user_input == "5":
                 contack_book = self._contact_book.display_contact_book()
                 for row in contack_book:
                     print(f"Name: {row['name']}\n"
                           f"Contact number: {row['contact_number']}\n"
                           f"Email Id: {row['email_id']}\n"
                           f"Address: {row['address']}", end='\n\n')
+                    print("<------------------------------->")
                 break
             else:
                 print("Invalid input")
