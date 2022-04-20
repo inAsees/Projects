f22e96a36c00f59c678fde852a3a4e5618861131
Gurasees Singh <gurasees.singh121@gmail.com>
2022-04-20 12:45:08 +0530

    Update random_password_generator.py
    1. Changes inside class PasswordGenerator:
        a. 3 instances created inside the __init__ method.
        b. New method created.(def is_password_already_exits(self, password_usage: str))
        c.  Inside method (def generate_password(self, password_usage: str, password_length: Optional[int]) ), datatype of password_length is changed to Optional[int].
    2. Changes inside class CliHandler:
        a. Handling of the newly created method(inside class PasswordGenerator) is done.(line 65 to 78)

--- random_password_generator.py
+++ random_password_generator.py
@@ -2,25 +2,32 @@ import random
 import string
 import os
 import csv
-from typing import Dict
+from typing import Dict, Optional
 
 
 class PasswordGenerator:
-    def __init__(self):
+    def __init__(self, parent_directory: str, resource_directory_path: str, resource_file_path: str):
+        self._parent_directory = parent_directory
+        self._resource_directory_path = resource_directory_path
+        self._resource_file_path = resource_file_path
         self._create_directory_if_not_exists()
         self._dic = self._load_from_file()
         self._minimum_length = 4
 
-    def generate_password(self, password_usage: str, password_length: int) -> str:
+    def is_password_already_exits(self, password_usage: str) -> bool:
+        if password_usage in self._dic:
+            return True
+
+    def generate_password(self, password_usage: str, password_length: Optional[int]) -> str:
         password = ""
-        if password_length == 0:
+        if password_length is None:
             password_length = 12
         password = random.sample(string.ascii_letters + string.punctuation + string.digits, k=password_length)
         self._dic[password_usage] = "".join(password)
         return "".join(password)
 
     def save(self) -> None:
-        with open(r"C:\Users\DELL\Desktop\my_passwords_repository\passwords_list.csv", "w", newline="") as f:
+        with open(self._resource_file_path, "w", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=["domain", "password"])
             writer.writeheader()
             for domain in self._dic:
@@ -29,32 +36,46 @@ class PasswordGenerator:
     def get_minimum_length(self) -> int:
         return self._minimum_length
 
-    @staticmethod
-    def _load_from_file() -> Dict:
+    def _load_from_file(self) -> Dict:
         dic = {}
 
-        if not os.path.exists(r"C:\Users\DELL\Desktop\my_passwords_repository\passwords_list.csv"):
+        if not os.path.exists(self._resource_file_path):
             return dic
 
-        with open(r"C:\Users\DELL\Desktop\my_passwords_repository\passwords_list.csv", "r") as f:
+        with open(self._resource_file_path, "r") as f:
             reader = csv.DictReader(f)
             for rows in reader:
                 dic[rows["domain"]] = rows["password"]
 
         return dic
 
-    @staticmethod
-    def _create_directory_if_not_exists() -> None:
-        if "my_passwords_repository" not in os.listdir(r'C:\Users\DELL\Desktop'):
-            os.mkdir(r"C:\Users\DELL\Desktop\my_passwords_repository")
-
+    def _create_directory_if_not_exists(self) -> None:
+        if "my_passwords_repository" not in os.listdir(self._parent_directory):
+            os.mkdir(self._resource_directory_path)
 
 class CliHandler:
     def start(self):
-        password_gen = PasswordGenerator()
+        password_gen = PasswordGenerator(r'C:\Users\DELL\Desktop\test_files',
+                                         r"C:\Users\DELL\Desktop\test_files\my_passwords_repository",
+                                         r"C:\Users\DELL\Desktop\test_files\my_passwords_repository\passwords_list.csv"
+                                         )
         password_usage = input("Password Generator Application.\n"
                                "Where are you going to use this password? "
                                "Example:Facebook,Instagram etc.\n")
+        if password_gen.is_password_already_exits(password_usage):
+            print(f"{password_usage} password already exists.\n"
+                  f"Do you want to replace it?\n"
+                  f"If YES press '1' or '2' if NO.\n")
+            while True:
+                user_input = input("Enter:")
+                if user_input == "2":
+                    print("App closed.")
+                    quit()
+                elif user_input == "1":
+                    break
+                else:
+                    print("Invalid input")
+                    continue
         while True:
             user_input = input(
                 f"Do you want to provide maximum length for the password?\n"
@@ -77,7 +98,7 @@ class CliHandler:
                 break
 
             elif user_input == "2":
-                print(f"Password is: {password_gen.generate_password(password_usage, password_length=0)}")
+                print(f"Password is: {password_gen.generate_password(password_usage, password_length=None)}")
                 break
             else:
                 print("Invalid input")
@@ -85,12 +106,18 @@ class CliHandler:
         print("Do you want to save the password?\n"
               "If YES press 1\n"
               "If NO press 2\n")
-        user_input = input()
-        if user_input == "1":
-            password_gen.save()
-            print("Password saved successfully")
-        elif user_input == "2":
-            print("Password not saved.")
+        while True:
+            user_input = input()
+            if user_input == "1":
+                password_gen.save()
+                print("Password saved successfully")
+                break
+            elif user_input == "2":
+                print("Password not saved.")
+                break
+            else:
+                print("Invalid input")
+                continue
 
 
 if __name__ == "__main__":
