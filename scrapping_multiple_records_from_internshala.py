2c9a9f7670c166e3a1ad8727334051507ef8289b
Gurasees Singh <gurasees.singh121@gmail.com>
2022-04-26 20:49:42 +0530

    Major changes done.
    1.	New methods added,
    	Get_incentive
    	Get_Apply_by
    	Get_number_of_openings
    	Get_skill_set
    	Get_perks
    2.	New columns added,
    	Apply_by
    	Applicants
    	Number_of_openings
    	Perks
    3.	Skill_set logic changed and improved.

--- scrapping_multiple_records_from_internshala.py
+++ scrapping_multiple_records_from_internshala.py
@@ -4,6 +4,7 @@ from typing import List
 import requests as req
 from bs4 import BeautifulSoup as bs
 from bs4.element import ResultSet
+from tqdm import tqdm
 
 
 @dataclass
@@ -14,35 +15,34 @@ class CompanyInfo:
     incentive: str
     duration: str
     location: str
-    skill_set: List[str]
+    apply_by: str
+    applicants: str
+    number_of_openings: int
+    skill_set: list[str]
+    perks: list[str]
     link: str
 
 
 class ScrapInternshala:
     def __init__(self):
         self._base_url = "https://internshala.com"
-        _python_intern_page = "internships/computer%20science-internship"
+        _python_intern_page = "internships/keywords-python"
         self._python_internship_page_url = ["{}/{}/page-{}".format(self._base_url, _python_intern_page, i) for i in
-                                            range(1, 48)]
+                                            range(1, 4)]
         self._company_info_list = []  # type: List[CompanyInfo]
 
     def scrap_all_pages(self) -> None:
-        page_no = 1
         for url in self._python_internship_page_url:
-            print("Page {} scrapping begins....".format(page_no))
             self._scrap_url(url)
-            print("\n")
-            print("Page {} scrapping finished....".format(page_no))
-            page_no += 1
 
     def dump(self, file_path: str):
         with open(file_path, "w", encoding="utf-8", newline="") as f:
             writer = csv.DictWriter(f,
                                     fieldnames=["job_title", "company", "lump_sum_per_month", "incentive",
-                                                "duration", "location", "skill_set", "link", ])
+                                                "duration", "location", "apply_by", "applicants", "number_of_openings",
+                                                "skill_set", "perks", "link", ])
             writer.writeheader()
-            print("Dumping begins....")
-            for ele in self._company_info_list:
+            for ele in tqdm(self._company_info_list, desc="Dumping..."):
                 writer.writerow(
                     {"job_title": ele.job_title,
                      "company": ele.company,
@@ -50,83 +50,129 @@ class ScrapInternshala:
                      "incentive": ele.incentive,
                      "duration": ele.duration,
                      "location": ele.location,
+                     "apply_by": ele.apply_by,
+                     "applicants": ele.applicants,
+                     "number_of_openings": ele.number_of_openings,
                      "skill_set": ele.skill_set,
+                     "perks": ele.perks,
                      "link": ele.link})
-            print("Dumping finished....")
 
     def _scrap_url(self, url: str) -> None:
         page_src = req.get(url).text
         page_soup = bs(page_src, "html.parser")
         companies_box = page_soup.findAll("a", {"class": "view_detail_button"})
 
-        for company in companies_box:
+        for company in tqdm(companies_box, desc="Scrapping companies..."):
             details_url = self._base_url + company["href"]
             company_details_src = req.get(details_url).text
             company_details_soup = bs(company_details_src, "html.parser")
             company_info = self._parse_company_info(company_details_soup, details_url)
             self._company_info_list.append(company_info)
-            print(">", end="")
 
     @classmethod
     def _parse_company_info(cls, company_soup: bs, detail_url: str) -> CompanyInfo:
-        link = detail_url
-        company = company_soup.find("a", {"class": "link_display_like_text"}).text.strip()
         job_title = company_soup.find("span", {"class": "profile_on_detail_page"}).text.strip()
-        incentive_if_any = company_soup.findAll("i")
-        stipend = cls._get_stipend(company_soup.find("span", {"class": "stipend"}).text, incentive_if_any)
+        company = company_soup.find("a", {"class": "link_display_like_text"}).text.strip()
+        stipend = cls._get_stipend(company_soup.find("span", {"class": "stipend"}).text)
+        incentive = cls._get_incentive(company_soup.findAll("i"))
         duration = cls._get_duration(company_soup.findAll("div", {"class": "item_body"}))
         location = company_soup.find("a", {"class": "location_link"}).text.strip()
-        skill_set = "Not mentioned"
-        try:
-            skill_set = company_soup.find("div", {"class": "round_tabs_container"}).get_text().strip().split()
-        except AttributeError as e:
-            pass
+        apply_by = cls._get_apply_by(company_soup.findAll("div", {"class": "item_body"}))
+        applicants = company_soup.find("div", {"class": "applications_message"}).text.strip()
+        number_of_openings = cls._get_number_of_openings(company_soup.findAll("div", {"class": "text-container"}))
+        skill_set = cls._get_skills_set(company_soup)
+        perks = cls._get_perks(company_soup)
+        link = detail_url
 
-        return CompanyInfo(job_title, company, stipend[0], stipend[1], duration, location, skill_set, link)
+        return CompanyInfo(job_title, company, stipend, incentive, duration, location, apply_by, applicants,
+                           number_of_openings, skill_set, perks, link)
 
     @staticmethod
-    def _get_stipend(raw_text: str, incentive_if_any: ResultSet) -> tuple[int, str]:
+    def _get_perks(company_soup: bs) -> list[str]:
+        perks = []
+        perks_result_set = company_soup.findAll("div", {"class": "section_heading heading_5_5"})
+        for heading in perks_result_set:
+            if "Perks" in heading.text:
+                children = heading.find_next("div", {"class": "round_tabs_container"}).children
+                for perk in children:
+                    if perk != "\n":
+                        perks.append(perk.text)
+                return perks
+        return ["Not mentioned"]
+
+    @staticmethod
+    def _get_skills_set(company_soup: bs) -> list[str]:
+        skill_set = []
+        skill_result_set = company_soup.findAll("div", {"class": "section_heading heading_5_5"})
+        for heading in skill_result_set:
+            if "Skill(s) required" in heading.text:
+                children = heading.find_next("div", {"class": "round_tabs_container"}).children
+                for skill in children:
+                    if skill != "\n":
+                        skill_set.append(skill.text)
+                return skill_set
+        return ["Not mentioned"]
+
+    @staticmethod
+    def _get_number_of_openings(company_soup: ResultSet) -> int:
+        for i in company_soup:
+            try:
+                if type(int(i.text)) is int:
+                    return int(i.text)
+            except:
+                pass
+
+    @staticmethod
+    def _get_apply_by(company_soup: ResultSet) -> str:
+        for apply in company_soup:
+            if "'" in apply.text:
+                return apply.text.strip()
+
+    @staticmethod
+    def _get_incentive(company_soup: ResultSet) -> str:
         incentive = "0"
-        for i in incentive_if_any:
+        for i in company_soup:
             try:
                 text = i.get("popover_content")
                 if "starting" in text:
                     continue
                 elif "%" in text:
                     idx = text.index("(")
-                    incentive = text[idx + 1:]
+                    incentive = text[idx + 1:-2]
                 else:
                     idx = text.index("(")
-                    incentive = text[idx + 3:]
+                    incentive = text[idx + 3:-2]
             except:
                 pass
+        return incentive
 
+    @staticmethod
+    def _get_stipend(raw_text: str) -> int:
         salary = "".join(raw_text.lstrip().split(" /month"))
         if len(salary) < 6:
-            return int(salary), "0"
+            return int(salary)
         elif len(salary) > 5:
             if "-" in raw_text and " lump sum" in raw_text:
                 raw_salary = raw_text.split(" lump sum")
                 raw_salary = raw_salary[0].split("-")
                 avg = (int(raw_salary[0]) + int(raw_salary[1])) // 2
-                return int(avg), "0"
+                return int(avg)
             elif "-" in salary:
                 salary = list(map(int, salary.split("-")))
                 avg = (salary[0] + salary[1]) // 2
-                return int(avg), "0"
+                return int(avg)
             elif " lump sum +  Incentives" in salary:
                 salary = "".join(salary.split(" lump sum +  Incentives"))
-                return int(salary), incentive
+                return int(salary)
             elif " +  Incentives" in salary:
                 salary = "".join(salary.split(" +  Incentives"))
-                return int(salary), incentive
+                return int(salary)
             elif " lump sum" in salary:
                 salary = "".join(salary.split(" lump sum"))
-                return int(salary), "0"
+                return int(salary)
             else:
                 salary = 0
-                incentive = "0"
-                return salary, incentive
+                return salary
 
     @staticmethod
     def _get_duration(raw_text: ResultSet) -> str:
@@ -138,4 +184,4 @@ class ScrapInternshala:
 if __name__ == "__main__":
     scrapper = ScrapInternshala()
     scrapper.scrap_all_pages()
-    scrapper.dump(r"C:\Users\DELL\Desktop\scrap_for_computer_science.csv")
+    # scrapper.dump(r"C:\Users\DELL\Desktop\scrap_key_python.csv")
